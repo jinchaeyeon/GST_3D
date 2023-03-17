20b1fe3d60798f42202cefe40774e28ecfb48213
YUJIN-G <yjgo235@gmail.com>
2023-03-17 16:23:42 +0900

    animation 적용 테스트

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,6 +1,6 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useEffect, useRef } from "react";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { useGLTF, Stats } from "@react-three/drei";
+import { useGLTF, Stats, useAnimations } from "@react-three/drei";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { TextureLoader } from "three";
@@ -11,7 +11,8 @@ const Main: React.FC = () => {
       <ambientLight intensity={0.7} />
       <spotLight intensity={0.7} position={[1000, 1000, 1000]} />
       <directionalLight intensity={0.7} position={[0, 0, 5]} />
-      <OrbitControls autoRotate />
+      <OrbitControls />
+      {/* autoRotate */}
       <Suspense fallback={null}>
         <Model />
         {/* <Model2 /> */}
@@ -22,24 +23,35 @@ const Main: React.FC = () => {
 };
 
 const Model = () => {
-  //const gltf = useLoader(GLTFLoader, "./house/scene.gltf");
-  // const gltf = useLoader(GLTFLoader, "./shiba/scene.gltf");
   const gltf = useLoader(GLTFLoader, "./dna_lab_machine/scene.gltf");
 
-  // return <primitive object={gltf.scene} scale={0.008} />;
-  return <primitive object={gltf.scene} scale={1} physicallyCorrectLights />;
+  const { animations } = useGLTF("./dna_lab_machine/scene.gltf");
+  // Extract animation actions
+  const { ref, actions } = useAnimations(animations);
+
+  useEffect(() => {
+    if (actions["Scene"]) {
+      actions["Scene"].play();
+    }
+  }, [actions]);
+
+  return (
+    <primitive
+      ref={ref}
+      object={gltf.scene}
+      scale={1}
+      physicallyCorrectLights
+    />
+  );
 };
 const Model2 = () => {
-  //const gltf = useLoader(GLTFLoader, "./house/scene.gltf");
   const gltf = useLoader(GLTFLoader, "./shiba/scene.gltf");
 
-  // return <primitive object={gltf.scene} scale={0.008} />;
   return <primitive object={gltf.scene} scale={1} physicallyCorrectLights />;
 };
 const Model3 = () => {
   const gltf = useLoader(GLTFLoader, "./wooden_bridge_pack/scene.gltf");
 
-  // return <primitive object={gltf.scene} scale={0.008} />;
   return <primitive object={gltf.scene} scale={1} physicallyCorrectLights />;
 };
 
