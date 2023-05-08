95f44f0c3b6da7325e476c4f7d3b3b9fc88602e5
YUJIN-G <yjgo235@gmail.com>
2023-05-09 08:51:04 +0900

    3D로더의 프로그레스바에 클립로더 추가

--- src/components/ThreeDModelLoader.tsx
+++ src/components/ThreeDModelLoader.tsx
@@ -1,13 +1,22 @@
 import { Html, useProgress } from "@react-three/drei";
 import { ProgressBar } from "@progress/kendo-react-progressbars";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function ThreeDModelLoader() {
   const { progress } = useProgress();
 
   return (
-    <Html center>
+    <Html
+      center
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <ClipLoader color="#2184bb" size={150} loading={true} />
       <ProgressBar
-        style={{ width: "350px", height: "22px" }}
+        style={{ width: "350px", height: "22px", marginTop: "50px" }}
         value={progress}
         label={(props) => {
           return (
